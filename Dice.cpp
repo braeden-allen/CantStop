c294c9077fc0cc40700668e41f87efeec4224035
Braeden Allen <141491180+braeden-allen@users.noreply.github.com>
2025-01-23 16:14:08 -0500

    Add files via upload

---
+++ Dice.cpp
@@ -0,0 +1,5 @@
+//
+// Created by braeden on 1/23/25.
+//
+
+#include "Dice.h"
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+/**
+ * Braeden Allen
+ * Mateusz W
+ */
+
+int main() {
+
+    std::cout << "Hello World";
+
+
+    return 0;
+}
