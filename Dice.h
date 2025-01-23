866d30d77646b167f70cb5812bef667f7f37cb5f
braeden <braeden.allen23@gmail.com>
2025-01-23 16:40:53 -0500

    Messing around w this, Building out Dice.h

--- Dice.h
+++ Dice.h
@@ -5,10 +5,23 @@
 #ifndef CANTSTOP_DICE_H
 #define CANTSTOP_DICE_H
 
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
 
 class Dice {
-
+    private:
+        int nDice;
+        int* diceValues;
+    public:
+        Dice(int n){};
+        ~Dice();
+        ostream& print(ostream&);
+        const int* roll();
 };
 
+inline ostream& operator << ( ostream&, Dice& );
 
 #endif //CANTSTOP_DICE_H
