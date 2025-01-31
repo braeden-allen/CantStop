1729bca5c19e14f41b31e6acb075c909c31ca1aa
braeden <braeden.allen23@gmail.com>
2025-01-31 12:55:05 -0500

    Made the changes from her email. Need to remove UnitTest Class for whatever reason

--- main.cpp
+++ main.cpp
@@ -4,17 +4,48 @@
 //Authors: Braeden and Mateusz
 //----------------------------------------
 #include "Dice.h"
-#include "UnitTest.h"
 #include "tools.hpp"
 //----------------------------------------
+
+void unitDice();
 
 int main(int argc , char* argv[]) {
     banner();
 
-    UnitTest test;
-
-    test.unitDice(); //used to test out dice class
+    unitDice(); //used to test out dice class
 
     bye();
     return 0;
+}
+
+void unitDice() {
+    ofstream outFile("output.txt" , ios::app);
+    if (!outFile){cerr << "Error Opening Output.txt File"; return;}
+
+    //initial message
+    outFile << "START OF DICE TEST" << endl;
+    Dice d1(6); //roll different numbers of dice each time
+
+    //test the roll function
+    outFile << "TEST FOR ACCURATE ROLL" << endl;
+    const int* dieValues = d1.roll();
+    outFile << "Values from roll: ";
+
+    for (int k = 0; k < d1.getnDice(); k++) {
+        outFile << dieValues[k] << " ";
+
+        if (dieValues[k] < 1 || dieValues[k] > 6){outFile << "n\nValues are out of range(1-6)" << endl;}
+    }
+
+    //test the print function
+    outFile << "\nTest for accurate PRINTING of Dice values" << endl;
+    outFile << "Die Values: ";
+    d1.print(outFile);
+    outFile << endl;
+
+    //test the destructor for memory leaks
+    outFile << "DESTRUCTOR TEST" << endl;
+    Dice d2(4);
+    outFile << "Class destructed without error or memory leak\n" << endl;
+    outFile.close();
 }
