35947c2f394804c91635cd7b2697bc578be1bc65
Schnoww <96844872+Matt-Wiszniewski@users.noreply.github.com>
2025-02-05 17:11:56 -0500

    Update Player.h
    I think I am all good with this one,. Just need editing for Player.cpp file

--- Player.h
+++ Player.h
@@ -1,1 +1,36 @@
+//----------------------------------------
+//File: Player.hpp
+//Authors: Braeden and Mateusz
+//----------------------------------------
+#pragma once
+#include "tools.hpp"
+#include "enums.hpp"
+#include <ostream>
+//----------------------------------------
 
+using namespace std;
+
+
+class Player{
+    private:
+        string name;
+        ECcolor color;
+        int score;
+        int scoreboard[3];
+    public:
+    //consturctor
+        Player(string playerName, ECcolor playerColor);
+        ~Player();
+        ECcolor getColor() const;
+
+        int score() const;
+        int getScore() const;
+
+        //store colmn num in next available spot
+        bool wonColumn( int colNum);
+
+        void print() const;
+        
+        friend ostream& operator << (ostream& os, const Player& player);
+
+};
