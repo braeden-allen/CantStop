6271e6e1afac2916b63f5bf84485ebed75ad0bfa
Schnoww <96844872+Matt-Wiszniewski@users.noreply.github.com>
2025-02-05 17:10:09 -0500

    Update enums.hpp
    This is completely done

--- enums.hpp
+++ enums.hpp
@@ -1,1 +1,18 @@
+//----------------------------------------
+//File: enums.hpp
+//Authors: Braeden and Mateusz
+//----------------------------------------
+#pragma once
+#include "tools.hpp"
+
+enum class ECcolor { 
+    White, 
+    Orange, 
+    Yellow, 
+    Green, 
+    Blue 
+};
+
+extern const char* colorNames[];
+
 
