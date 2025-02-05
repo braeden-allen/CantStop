4f4cf6683ce4d8bbf61298e559fa35ad224257b7
Schnoww <96844872+Matt-Wiszniewski@users.noreply.github.com>
2025-02-05 17:11:11 -0500

    Update Player.cpp
    Lots of errors, might be something small I am missing from OOP

--- Player.cpp
+++ Player.cpp
@@ -1,1 +1,61 @@
+//----------------------------------------
+//File: Player.cpp
+//Authors: Braeden and Mateusz
+//----------------------------------------
+#include "Player.hpp"
+#include "enums.hpp"
+#include "tools.hpp"
+#include <ostream>
+//----------------------------------------
+
+
+Player::Player(const string& playerName, ECcolor color) 
+    //initializer list
+    : name(playerName), playerColor(color), score(0), columnCount(0) {
+        for(int j = 0; j < 3; j++){
+            //reason for -1 is to make sure no columns captured
+            scoreboard[j] = -1; 
+        }
+    }
+
+Player::~Player(){
+
+}
+
+ECcolor Player::color() const {
+    return playerColor;
+}
+
+int Player::score() const {
+    return score;
+}
+
+bool Player::wonColumn(int colNum){
+    if (columnCount < 3) {
+        scoreboard[columnCount++] = colNum;
+        score++;
+        
+        //check fi player has won 3 columns
+        return columnCount == 3;
+    }
+    return false;
+}
+
+void Player::print() const {
+    cout << "Player: " << name << endl;
+    cout << "Color: " << colorNames[(int)playerColor] << endl;
+    cout << "Score: " << score << endl;
+}
+
+
+//overload
+ostream& operator << (ostream& os, const Player& player){
+    os << "Player: " << player.name << "\n" 
+    << "Color: " << colorNames[(int)player.playerColor] << "\n"
+    << "Score: " << player.score << "\n";
+    return os;
+
+}
+
+
 
