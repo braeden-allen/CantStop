076bd83b9bedf3f7f6acc6c024b2b6bbd970ff25
Schnoww <96844872+Matt-Wiszniewski@users.noreply.github.com>
2025-02-05 17:10:37 -0500

    Update enums.cpp
    This is completely done

--- enums.cpp
+++ enums.cpp
@@ -1,1 +1,17 @@
+//----------------------------------------
+//File: enums.cpp
+//Authors: Braeden and Mateusz
+//----------------------------------------
+
+#include "tools.hpp"
+#include "enums.hpp"
+
+const char* colorNames[] = {
+    "White",
+    "Orange",
+    "Yellow",
+    "Green",
+    "Blue"
+};
+
 
