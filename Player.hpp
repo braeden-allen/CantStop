f1136fc2a4ca981f7bcf241f3618f1c5b542dc09
Schnoww <96844872+Matt-Wiszniewski@users.noreply.github.com>
2025-02-11 19:46:48 -0500

    Create Player.hpp
    New .hpp file

---
+++ Player.hpp
@@ -0,0 +1,33 @@
+//----------------------------------------
+//File: Player.hpp
+//Authors: Braeden and Mateusz
+//----------------------------------------
+#pragma once
+#include "tools.hpp"
+#include "enums.hpp"
+//----------------------------------------
+
+class Player{
+    private:
+        string playerName;
+        ECcolor playerColor;
+        int scoreboard[3];
+        int columnCount;
+        int score = 0;
+
+    public:
+        //constructor
+        explicit Player(const string Name, ECcolor color);
+
+        //better spot for defaulting for player
+        ~Player() = default;
+
+        // Special functions
+        ECcolor getColor() const;
+        int getScore() const;
+
+        //store column num in next available spot
+        bool wonColumn(int colNum);
+        ostream& print(ostream& os) const;
+};
+ostream& operator << (ostream&, const Player&);
