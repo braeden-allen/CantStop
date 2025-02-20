0484f646eeca29ae2ad2f0cff26218715caa6c85
Schnoww <96844872+Schnoww@users.noreply.github.com>
2025-02-20 12:41:48 -0500

    Update Game.hpp

--- Game.hpp
+++ Game.hpp
@@ -1,1 +1,26 @@
+//----------------------------------------
+//File: Game.hpp
+//Authors: Braeden and Mateusz
+//----------------------------------------
 
+#include "Player.hpp"
+#include "Dice.hpp"
+#include "Column.hpp"
+#include "tools.hpp"
+
+class Game {
+private:
+    Dice* dice;
+    Player player1;
+    Player player2;
+
+    Column column1;
+    Column column2;
+
+public:
+    Game();
+    ~Game();
+
+    void getNewPlayer();
+
+}
