585f4748cab955163d9ba3635a118f9f3dc37eb0
Schnoww <96844872+Schnoww@users.noreply.github.com>
2025-02-25 12:35:21 -0500

    Update Board.hpp

--- Board.hpp
+++ Board.hpp
@@ -1,1 +1,34 @@
 
+//----------------------------------------
+//File: Board.hpp
+//Authors: Braeden and Mateusz
+//----------------------------------------
+
+#pragma once
+#include "tools.hpp"
+#include "Player.hpp"
+#include "enums.hpp"
+#include "Dice.hpp"
+#include "Game.hpp"
+
+class Board {
+private:
+        int towerCounter;                         // T
+        int towerColumns[3];                      
+        Column* backBone[13];                     
+        Player* currentPlayer;                    
+
+    public:
+        Board();                                  
+        ~Board();                                
+        void print(std::ostream& os) const;      
+        void startTurn(Player* player);           /
+        bool move(int column);                  
+        void stop();                             
+        void bust();                           
+        friend std::ostream& operator<<(std::ostream& os, const Board& board); 
+
+
+
+
+}
